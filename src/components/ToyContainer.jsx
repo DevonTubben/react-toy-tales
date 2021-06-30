1bca1e5ec641b22ad909eac3f4a86fca9773aa77
Devon Tubben <tubbend1@msu.edu>
2021-06-30 18:18:35 -0400

    pod meeting

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from 'react'
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
+
+class ToyContainer extends React.Component { 
+ constructor() { 
+   super()
+   this.state={ 
+     toys:[]
+   }
+ }
+ componentDidMount(){ 
+   fetch("http://localhost:3000/toys")
+   .then(res => res.json())
+   .then(toys => this.setState({ 
+     toys: toys
+   }))
+ }
+  render() {
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {this.state.toys.map(toy => < ToyCard toy={toy}/>)}
     </div>
   );
 }
-
+}
 export default ToyContainer;
